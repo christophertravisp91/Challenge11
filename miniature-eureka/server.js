1853d36362c03410ee77a712b2bae10f9e7b84eb
TomFoolery <christophertravisp91@gmail.com>
2022-01-23 13:59:31 -0500

    added get/post requests

--- miniature-eureka/server.js
+++ miniature-eureka/server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
 // Server set-up
 const app  = express();
 
@@ -11,30 +15,37 @@ app.use(express.static("./develop/public"));
 
 // "Get" Request
 app.get("/api/notes", (req,res) => {
-    readFileAsync(".develop/db/db.json", "utf8").then(function(data) {
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
 });
 
 // "Post" Request
-app.post("/api/notes", (req,res) => {
-
-})
-
-
+app.post("/api/notes", (req, res) => {
+    const note = req.body;
+    readFileAsync("./develop/db/db.json", "utf8").then(function(data) {
+        const notes = [].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    }).then(function(notes) {
+        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
+        res.json(note);
+    })
+});
 
 
 // HTML Routes
-app.get("/notes",  (req,res) => {
+app.get("/notes",  (req, res) => {
     res.sendFile(path.join(__dirname, "develop/public/notes.html"));
 });
 
-app.get("/",  (req,res) => {
+app.get("/",  (req, res) => {
     res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
-app.get("*",  (req,res) => {
+app.get("*",  (req, res) => {
     res.sendFile(path.join(__dirname, "develop/public/index.html"));
 });
 
